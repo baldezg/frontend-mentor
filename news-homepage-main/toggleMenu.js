deaa92d2d5e8782ba760a0a2102ab2a11e69918d
baldezg <gabrielbaldez34@gmail.com>
2023-10-25 12:03:21 -0300

    mobile menu

---
+++ news-homepage-main/toggleMenu.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector(".header__links");
+const navToggle = document.querySelector(".header__links-toggle");
+
+navToggle.addEventListener("click", () => {
+    
+    const visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+})
